f70e070f0888f7a3418628dc893e158911bd3780
Luca <lmatant737@g.educaand.es>
2026-03-10 19:56:12 +0100

    Modifico funciones del script

--- funciones.sh
+++ funciones.sh
@@ -63,6 +63,33 @@ FIN
 
 }
 
+#Funcion que permite ver si hay un paquete instalado o no
+
+f_paquete_instalado() {
+    dpkg -s "$1" &> /dev/null
+
+    if [ $? -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+#Comprueba que se han introducido argumentos al script
+
+f_parametros() {
+    if [ $# -eq 0 ]; then
+        echo "Error: no se han introducido argumentos."
+        exit 1
+    fi
+}
+
+f_parametros "$@"
+
+echo "Argumentos recibidos: $@"
+
+
 
 #--------------Zona  CODIGO PRINCIPAL SCRIPT--------------#
 
