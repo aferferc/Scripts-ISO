388a4d9f145c67ba5173ea3629551206c28450bb
Luca <lmatant737@g.educaand.es>
2026-03-12 10:43:16 +0100

    shfmt hecho

--- funciones.sh
+++ funciones.sh
@@ -1,10 +1,7 @@
-
 #!/usr/bin/env bash
 #Descripcion: Scrip con multiples Funciones utiles
 #Autor: Luca, Alfredo, Dani Berzosa, Frank
 #Version: 3
-
-
 
 #-------------------------COLORES-------------------------#
 ROJO='\033[0;31m'
@@ -21,14 +18,12 @@ NC='\033[0m' # No Color (restablecer)
 
 #--------------Zona declaracion de VARIABLES--------------#
 
-
-
 #--------------Zona declaracion de FUNCIONES--------------#
 
 #Descripcion f_ranking: Muestra un ranking de los 10 comandos mas usados del historial
 #No tiene entradas ni salidas
 
-f_ranking(){
+f_ranking() {
   if [ -r ~/.bash_history ]; then
     historial=$(cat ~/.bash_history | awk '{print $1}' | sort | uniq -c | sort -nr | head)
     echo "Los comandos mas usados son:"
@@ -43,7 +38,7 @@ f_ranking(){
 #Descripcion f_ayuda: Muestra la ayuda del script
 #No tiene entradas  ni salidas
 
-f_ayuda(){
+f_ayuda() {
 
   cat <<FIN
 Scrip de funciones utiles multiples, sintaxtis: . funciones.sh [param]
@@ -71,7 +66,7 @@ FIN
 
 f_paquete_instalado() {
 
-  dpkg -s "$1" &> /dev/null
+  dpkg -s "$1" &>/dev/null
 
   if [ $? -eq 0 ]; then
     return 0
@@ -79,7 +74,6 @@ f_paquete_instalado() {
     return 1
   fi
 }
-
 
 #Comprueba que se han introducido argumentos al script, para ello revisa si el argumento 1 esta vacio
 #Devuelve 0 si se an introducido, uno si no
@@ -91,13 +85,12 @@ f_parametros() {
   fi
 }
 
-
 #Devuelve la dirección IP de la máquina
 #Datos de entrada: ninguno, en un futuro indicamos si la tarjeta es inalámbrica
 #o cableada
 #Datos de salida: dirección IP
 
-f_obtener_ipv4(){
+f_obtener_ipv4() {
 
   ip a | grep -E $1 | grep -Eo '([0-9]{1,3}\.){3}([0-9]){1,3}' | head -n 1
 
@@ -118,10 +111,9 @@ f_bin_instalado() {
 #Datos de entrada:ninguno
 #Datos de salida: cero en caso de tener red y uno en caso contrario
 
+f_hay_conexion() {
 
-f_hay_conexion(){
-
-  ping -c 1 8.8.8.8 &> /dev/null
+  ping -c 1 8.8.8.8 &>/dev/null
 
   if [ $? -eq 0 ]; then
     return 0
@@ -133,12 +125,11 @@ f_hay_conexion(){
 #Comprueba la uid del usuario que esta usando el script
 #No recibe ni devuelve ningun parametro
 
-f_uid(){
+f_uid() {
 
   uid=$(id -u)
   return $uid
 }
-
 
 #Devuelve cero en caso de estar validado en la shell como root,
 #uno en caso contrario.
@@ -146,7 +137,7 @@ f_uid(){
 #Datos de salida: cero si esta validad en la shell como root
 # y uno en caso contrario.
 
-f_eres_root(){
+f_eres_root() {
   uid=$(f_uid)
   echo $uid
   if [ $uid -eq 0 ]; then
@@ -166,20 +157,19 @@ f_paquete_disponible() {
     return 0
   else
     return 1
- fi
+  fi
 }
 
 #Comprueba el nombre del paquete al que pertenece el binario que pasa como argumento
 
-f_buscar_paquetes(){
+f_buscar_paquetes() {
 
-if dpkg -s $(which "$1") >/dev/null 2>&1; then
+  if dpkg -s $(which "$1") >/dev/null 2>&1; then
     return 0
   else
     return 1
- fi
+  fi
 }
-
 
 #--------------Zona  CODIGO PRINCIPAL SCRIPT--------------#
 
@@ -189,43 +179,52 @@ if [ $? -eq 1 ]; then
   f_ayuda
 else
   case "$1" in
-    "r")
-      f_ranking;;
-    "h")
-      f_ayuda;;
-    "pi")
-      if [ $(f_paquete_instalado "$2") -eq 0]; then
-        echo "El paquete esta instalado"
-      else
-        echo "El paquete no esta instalado"
-      fi;;
-    "ipv4")
-      f_obtener_ipv4 $2;;
-    "u")
-      echo $(f_uid);;
-    "bi")
-      if [ $(f_bin_instalado "$2") -eq 0]; then
-        echo "El binario esta instalado"
-      else
-        echo "El binario no esta instalado"
-      fi;;
-    "pd")
-      if [ $(f_paquete_disponible "$2") -eq 0 ]; then
-        echo "El paquete esta disponible"
-      else
-        echo "El paquete no esta disponible"
-      fi;;
-    "ro")
-      if [ $(f_eres_root) -eq 0 ]; then
-        echo "Eres root"
-      else
-        echo "No eres root"
-      fi;;
-    "c")
-      if [ $(f_hay_conexion) -eq 0 ]; then
-        echo "Hay conexion"
-      else
-        echo "No hay conexion"
-      fi;;
+  "r")
+    f_ranking
+    ;;
+  "h")
+    f_ayuda
+    ;;
+  "pi")
+    if [ $(f_paquete_instalado "$2") -eq 0]; then
+      echo "El paquete esta instalado"
+    else
+      echo "El paquete no esta instalado"
+    fi
+    ;;
+  "ipv4")
+    f_obtener_ipv4 $2
+    ;;
+  "u")
+    echo $(f_uid)
+    ;;
+  "bi")
+    if [ $(f_bin_instalado "$2") -eq 0]; then
+      echo "El binario esta instalado"
+    else
+      echo "El binario no esta instalado"
+    fi
+    ;;
+  "pd")
+    if [ $(f_paquete_disponible "$2") -eq 0 ]; then
+      echo "El paquete esta disponible"
+    else
+      echo "El paquete no esta disponible"
+    fi
+    ;;
+  "ro")
+    if [ $(f_eres_root) -eq 0 ]; then
+      echo "Eres root"
+    else
+      echo "No eres root"
+    fi
+    ;;
+  "c")
+    if [ $(f_hay_conexion) -eq 0 ]; then
+      echo "Hay conexion"
+    else
+      echo "No hay conexion"
+    fi
+    ;;
   esac
 fi
